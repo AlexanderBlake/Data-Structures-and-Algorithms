f494f2b9d9054375ea2a7b200d8f00fc6014cd59
Alexander Blake <alex.blake24@gmail.com>
2020-12-16 17:31:07 -0800

    Update Modeling

--- modeling.js
+++ modeling.js
@@ -1,5 +1,6 @@
-let POPSIZE = 5;
+let POPSIZE = 1000;
 let CANSIZE = 2;
+let NUMELECTIONS = 10;
 
 class Citizen {
     constructor() {
@@ -13,14 +14,14 @@ class Citizen {
         } else if (norm < 95) {
             this.x = Math.random() * 27 + 68;
 
-            if (Math.random() * 1) {
+            if (Math.random() * 1 > 0.5) {
                 this.x *= -1;
             }
 
         } else {
             this.x = Math.random() * 5 + 95;
 
-            if (Math.random() * 1) {
+            if (Math.random() * 1 > 0.5) {
                 this.x *= -1;
             }
         }
@@ -30,26 +31,31 @@ class Citizen {
         } else if (norm < 95) {
             this.y = Math.random() * 27 + 68;
 
-            if (Math.random() * 1) {
+            if (Math.random() * 1 > 0.5) {
                 this.y *= -1;
             }
 
         } else {
             this.y = Math.random() * 5 + 95;
 
-            if (Math.random() * 1) {
+            if (Math.random() * 1 > 0.5) {
                 this.y *= -1;
             }
         }
     }
 }
 
-class Candidate {
+class Party {
     constructor(name) {
         this.name = name;
+        this.x = 0;
+        this.y = 0 ;
+        this.votes = 0;
+    }
+
+    newCandidate(x, y) {
         this.x = Math.random() * 200 - 100;
         this.y = Math.random() * 200 - 100;
-        this.votes = 1;
     }
 
     acceptVote() {
@@ -64,7 +70,7 @@ class Nation {
 
         for (let i = 0; i < POPSIZE; i++) {
             if (i < CANSIZE) {
-                this.candidates.push(new Candidate("Candidate " + String.fromCharCode(65 + i)));
+                this.candidates.push(new Party("Party " + String.fromCharCode(65 + i)));
             }
             this.population.push(new Citizen());
         }
@@ -85,24 +91,56 @@ class Nation {
         return Math.sqrt(Math.pow(person1.x - person2.x, 2), Math.pow(person1.y - person2.y, 2));
     }
 
-    election() {
-        let distJ;
-        let temp;
+    shift() {
+        let choice;
 
         for (let i = 0; i < POPSIZE; i++) {
-            distJ = [0, 0];
-            distJ[1] = this.distance(this.candidates[0], this.population[i]);
+            choice = this.determineVote(this.population[i]);
 
-            for (let j = 1; j < CANSIZE; j++) {
-                temp = this.distance(this.candidates[j], this.population[i]);
+            if (Math.random() * 1 > 0.5) {
+                this.population[i].x += this.candidates[choice].x
+                this.population[i].x /= 2;
 
-                if (temp < distJ[1]) {
-                    distJ[1] = temp;
-                    distJ[0] = j;
-                }
+                this.population[i].y += this.candidates[choice].y
+                this.population[i].y /= 2;
             }
+        }
+    }
 
-            this.candidates[distJ[0]].acceptVote();
+    determineVote(citizen) {
+        let temp;
+        let dist = [0, 0];
+        dist[1] = this.distance(this.candidates[0], citizen);
+
+        for (let i = 1; i < CANSIZE; i++) {
+            temp = this.distance(this.candidates[i], citizen);
+
+            if (temp < dist[1]) {
+                dist[1] = temp;
+                dist[0] = i;
+            }
+        }
+
+        return dist[0];
+    }
+
+    election() {
+        let vote;
+
+        for (let i = 0; i < CANSIZE; i++) {
+            this.candidates[i].votes = 1;
+            if (i == 0) {
+                this.candidates[i].newCandidate(Math.random() * 40 - 20, Math.random() * 40 - 20);
+            } else {
+                this.candidates[i].newCandidate(Math.random() * 200 - 100, Math.random() * 200 - 100);
+            }
+        }
+
+        for (let i = 0; i < POPSIZE; i++) {
+            vote = this.determineVote(this.population[i]);
+
+            // console.log(this.candidates[vote].name);
+            this.candidates[vote].acceptVote();
         }
 
         this.determineWinner();
@@ -116,15 +154,19 @@ class Nation {
             }
         }
 
-        console.log("Winner Winnner Chicken Dinner: " + this.candidates[winner].name);
+        console.log("Winner Winnner Chicken Dinner: " + this.candidates[winner].name + " with " + this.candidates[winner].votes + " votes");
     }
 }
 
 function main() {
     country = new Nation();
 
-    country.census();
-    country.election();
+    // country.census();
+    for (let i = 0; i < NUMELECTIONS; i++) {
+        country.election();
+        country.shift();
+    }
+    // country.census();
 }
 
 main();
