22b8f837c3cdc9581bba03472704459b9e7179e9
Alexander Blake <alex.blake24@gmail.com>
2021-04-20 16:04:09 -0700

    April 20th Session

---
+++ Algorithms/sorting.js
@@ -0,0 +1,68 @@
+// Time Complexity: O(n^2) Quadratic Time
+// Space Complexity: O(n) Total, O(1) Auxillary
+function selectionSort(arr) {
+    return arr;
+}
+
+// Time Complexity: O(n^2) Quadratic Time
+// Space Complexity: O(n) Total, O(1) Auxillary
+function insertionSort(arr) {
+    return arr;
+}
+
+// Time Complexity: O(n^2) Quadratic Time
+// Space Complexity: O(n) Total, O(1) Auxillary
+function bubbleSort(arr) {
+    let temp;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                arr[j] += arr[j + 1];
+                arr[j + 1] = arr[j] - arr[j + 1];
+                arr[j] -= arr[j + 1];
+            }
+
+            /*
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            */
+        }
+    }
+
+    return arr;
+}
+
+function arrayEqual(arr, arr2) {
+    if (arr.length != arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function main() {
+    let arr = bubbleSort([3, 5, 1, 2, 4]);
+    let expected = [1, 2, 3, 4, 5];
+
+    console.assert(arrayEqual(arr, expected), "Bubble Sort");
+
+    arr = selectionSort([3, 5, 1, 2, 4]);
+    expected = [1, 2, 3, 4, 5];
+    console.assert(arrayEqual(arr, expected), "Selection Sort");
+
+    arr = insertionSort([3, 5, 1, 2, 4]);
+    expected = [1, 2, 3, 4, 5];
+    console.assert(arrayEqual(arr, expected), "Insertion Sort");
+}
+
+main();
--- Data Structures/bst.js
+++ Data Structures/bst.js
@@ -6,16 +6,22 @@ class Node {
     }
 }
 
+// Space Complexity: O(n) Linear Space
+// Time Complexity: O(log n) Logarithmetic Time
+// Insert: O(log n)
+// Search: O(log n)
+// Delete: O(log n)
+
 class BST {
     constructor() {
         this.root = null;
     }
 
+    /*
     insert(value) {
         if (!this.root) {
             this.root = new Node(value);
-        }
-        else {
+        } else {
             let walker = this.root;
 
             while (walker) {
@@ -24,27 +30,108 @@ class BST {
                         walker = walker.left;
                     } else {
                         walker.left = new Node(value);
+                        break;
                     }
-                } else (value > walker.value) {
+                } else if (value > walker.value) {
                     if (walker.right) {
                         walker = walker.right;
                     } else {
                         walker.right = new Node(value);
+                        break;
                     }
                 }
             }
         }
     }
+    */
 
-    preOrderTraversal() {
-        return;
+    insert(value, node = null) {
+        if (node == null) {
+            if (!this.root) {
+                this.root = new Node(value);
+            }
+            node = this.root;
+        }
+
+        if (value < node.value) {
+            if (node.left) {
+                this.insert(value, node.left);
+            } else {
+                node.left = new Node(value);
+            }
+        } else if (value > node.value) {
+            if (node.right) {
+                this.insert(value, node.right);
+            } else {
+                node.right = new Node(value);
+            }
+        }
     }
 
-    postOrderTraversal() {
-        return;
+    postOrderTraversal(node = null) {
+        if (node == null) {
+            node = this.root;
+        }
+
+        if (node.left) {
+            this.postOrderTraversal(node.left);
+        }
+
+        if (node.right) {
+            this.postOrderTraversal(node.right);
+        }
+
+        console.log(node.value);
     }
 
-    inOrderTraversal() {
-        return;
+    preOrderTraversal(node = null) {
+        if (node == null) {
+            node = this.root;
+        }
+
+        console.log(node.value);
+
+        if (node.left) {
+            this.preOrderTraversal(node.left);
+        }
+
+        if (node.right) {
+            this.preOrderTraversal(node.right);
+        }
+    }
+
+    // Depth - First Traversal (DFS: Depth First Search)
+    inOrderTraversal(node = null) {
+        if (node == null) {
+            node = this.root;
+        }
+
+        if (node.left) {
+            this.inOrderTraversal(node.left);
+        }
+
+        console.log(node.value);
+
+        if (node.right) {
+            this.inOrderTraversal(node.right);
+        }
     }
 }
+
+function main() {
+    tree = new BST();
+    tree.insert(24);
+    tree.insert(48);
+    tree.insert(12);
+    tree.insert(96);
+    tree.insert(18);
+    tree.insert(6);
+    tree.insert(36);
+
+    // console.log(tree);
+    // tree.inOrderTraversal();
+    // tree.postOrderTraversal();
+    tree.preOrderTraversal();
+}
+
+main();
