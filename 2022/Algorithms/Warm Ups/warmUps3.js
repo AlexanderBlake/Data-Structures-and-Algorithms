8a55c510198fee1d7781c40fa42ce43c8c441d92
Alexander Blake <alex.blake24@gmail.com>
2022-02-09 16:16:22 -0800

    Feburary 9th Session

--- 2022/Algorithms/Warm Ups/warmUps3.js
+++ 2022/Algorithms/Warm Ups/warmUps3.js
@@ -21,6 +21,41 @@ function infiniteLoop()
     }
 }
 
+function minCoins(n)
+{
+    
+}
+
+function isPrime(n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    else if (n <= 3)
+    {
+        return true;
+    }
+    else
+    {
+        for (let i = 2; i < n; i++)
+        {
+            if (n % i === 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }   
+}
+
+function swap(a, b)
+{
+    let temp = a;
+    a = b;
+    b = temp;
+}
+
 function stackOverflow(i)
 {
     console.log(i)
