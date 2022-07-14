bced55600e16f2087a7202695fa5f3cd8434ac90
Alexander Blake <alex.blake24@gmail.com>
2022-07-14 12:02:56 -0700

    July 14th Session

---
+++ 2022/July/algos.js
@@ -0,0 +1,61 @@
+function nand(a, b) {
+
+}
+
+function nor(a, b) {
+
+}
+
+function xor(a, b) {
+
+}
+
+function xnor(a, b) {
+
+}
+
+function challenge1(x, y)
+{
+    /*
+    if (x)
+    {
+        y = false;
+    }
+    else
+    {
+        y = true;
+    }
+    */
+
+    y = !x;
+}
+
+function challenge2(x, y)
+{
+    /*
+    if (x)
+    {
+        return y;
+    }
+    else
+    {
+        return false;
+    }
+    */
+
+    return x && y;
+}
+
+function challenge3(x, z)
+{
+    /*
+    if (x)
+    {
+        y = z;
+    }
+    else
+    {
+        y = !z;
+    }
+    */
+}
