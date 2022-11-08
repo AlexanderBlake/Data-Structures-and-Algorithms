f28477ad9714ff75a499054f539e95bab5927943
Alexander Blake <alex.blake24@gmail.com>
2022-11-07 16:39:06 -0800

    November 7th Session

---
+++ 2022/Algorithms/Warm Ups/warmUps16.js
@@ -0,0 +1,67 @@
+function challenge1(x, y)
+{
+    /*
+    if (x)
+    {
+        y = false;
+    }
+    else
+    {
+        y = true;
+    }
+    */
+    y = !x;
+}
+
+function challenge2(x, y)
+{
+    /*
+    if (x)
+    {
+        return y;
+    }
+    else
+    {
+        return false;
+    }
+    */
+    return x && y;
+}
+
+function challenge3(x, y, z)
+{
+    /*
+    if (x)
+    {
+        y = z;
+    }
+    else
+    {
+        y = !z;
+    }
+    */
+    // y = (x && z) || (!x && !z)
+    y = x === z;
+}
+
+function nand(a, b)
+{
+    return !(a && b);
+}
+
+function nor(a, b)
+{
+    return !(a || b);
+}
+
+function xor(a, b)
+{
+    // return (!a && b) || (a && !b);
+    return a !== b;
+}
+
+function xnor(a, b)
+{
+    return a === b;
+}
+
