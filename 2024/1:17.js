25ede7e1be1c30d78af1e55daea797e0cbdb2747
Alexander Blake <alex.blake24@gmail.com>
2024-01-17 14:11:45 -0800

    January 17th Session

---
+++ 2024/1:17.js
@@ -0,0 +1,15 @@
+function main() {
+    for (let i = 2; i < n / 12; i = i + 7) {
+        for (let j = i + 2; j < n; j++) {
+            console.log("Try setting it to wumbo\n");
+        }
+    }
+
+    for (let i = 1; i < n * n; i = i * 10) {
+        for (let j = 0; j < n / 2; j++) {
+            console.log("Did you set it to wumbo?\n");
+        }
+    }
+}
+
+main();
