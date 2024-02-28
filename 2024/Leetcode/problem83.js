3fb35e153193312579d369ffd45d95b9946ec1e4
Alexander Blake <alex.blake24@gmail.com>
2024-02-28 12:31:14 -0800

    Problem 83 JS Solution

---
+++ 2024/Leetcode/problem83.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let current = head;
+
+    while (current && current.next) {
+        if (current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
