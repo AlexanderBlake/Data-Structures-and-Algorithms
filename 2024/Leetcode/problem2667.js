690b3860be3047a798452d3efa1a4a41016bf830
Alexander Blake <alex.blake24@gmail.com>
2024-02-29 11:02:40 -0800

    Problem 2667

---
+++ 2024/Leetcode/problem2667.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
