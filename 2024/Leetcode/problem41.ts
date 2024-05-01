f801124d1b2bc1f0ab23cdd5a182eb5f6456c412
Alexander Blake <alex.blake24@gmail.com>
2024-05-01 12:19:35 -0700

    First Missing Positive Multiple Solutions

--- 2024/Leetcode/problem41.ts
+++ 2024/Leetcode/problem41.ts
@@ -1,4 +1,53 @@
 function firstMissingPositive(nums: number[]): number {
+    // Time Complexity: O(n^2) Quadratic
+    // Space Complexity: O(1) Constant
+    /*
+    for (let i = 1; i <= nums.length; i++) {
+        let exists = false;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] == i) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            return i;
+        }
+    }
+    return nums.length + 1;
+    */
+
+    // Time Complexity: O(n log n)
+    // Space Complexity: O(n) Linear
+    /*
+    nums.sort((a, b) => a - b);
+
+    let val = 1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            if (nums[i] > val) {
+                return val;
+            } else if (nums[i] == val) {
+                val++;
+            }
+        }
+    }
+    return val;
+    */
+
+    // Time Complexity: O(n) Linear
+    // Space Complexity: O(n) Linear
+    /*
+    let mySet = new Set(nums);
+    for (let i = 1; i <= nums.length + 1; i++) {
+        if (!mySet.has(i)) {
+            return i;
+        }
+    }
+    */
+
+    // Time Complexity: O(n) Linear
+    // Space Complexity: O(1) Constant
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] <= 0) {
             nums[i] = nums.length + 1;
